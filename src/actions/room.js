9ede4fd6bd348707672acbc4cc7012b95e844277
Katsuki Fujimoto <kf1996cnbl@gmail.com>
2019-06-27 19:37:13 +0900

    Implement room details screen

--- src/actions/room.js
+++ src/actions/room.js
@@ -1,13 +1,21 @@
 import { HOST } from '../constants';
-import { normalizeRooms } from '../utils';
+import { normalizeRooms, normalizeRoom } from '../utils';
 // import console = require('console');
 
 export const SET_ROOMS = 'SET_ROOMS';
+export const SET_ROOM = 'SET_ROOM';
 
 export function setRooms(rooms) {
   return {
     type: SET_ROOMS,
     rooms
+  }
+}
+
+export function setRoom(room) {
+  return {
+    type: SET_ROOM,
+    room
   }
 }
 
@@ -20,6 +28,23 @@ export function getRooms() {
 
       if (json.is_success) {
         dispatch(setRooms(normalizeRooms(json.rooms)));
+      } else {
+        alert(json.error);
+      }
+    })
+    .catch(e => alert(e));
+  }
+}
+
+export function getRoom(roomId) {
+  return (dispatch) => {
+    return fetch(`${HOST}/api/v1/rooms/${roomId}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log("get a single room:", json);
+
+      if (json.is_success) {
+        dispatch(setRoom(normalizeRoom(json.room)));
       } else {
         alert(json.error);
       }
